502fac03eec771334eb8bff94b1f98e8e95f12cc
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-11-14 12:16:44 +0530

    Updated Code

--- Thu Lab/JavaScript/TaskManager/assets/javascripts/taskController.js
+++ Thu Lab/JavaScript/TaskManager/assets/javascripts/taskController.js
@@ -2,7 +2,7 @@
 // Controller - Connect View with Model
 // View -> Controller -> Model -> Logic/DB -> Controller -> View
 
-window.addEventListener("load", bindEvents);
+// window.addEventListener("load", bindEvents);
 
 function bindEvents() {
     document.querySelector("#addNewTaskBtn").addEventListener("click", addNewTask);
@@ -15,31 +15,83 @@ function addTaskItem(tagName, taskText) {
 }
 
 function addNewTask() {
-    var taskTitle = document.querySelector("#box-1").value;
-    var taskDesc = document.querySelector("#box-2").value;
-    var startDate = document.querySelector("#box-3").value;
-    var endDate = document.querySelector("#box-4").value;
-    var ul = document.querySelector("#taskList");
-    var li = document.createElement("li");
-
-    var deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete Task";
-    deleteBtn.className = "btn btn-danger";
-
-    var editBtn = document.createElement("button");
-    editBtn.innerText = "Edit Task";
-    editBtn.className = "btn btn-warning";
+    var taskTitle = document.querySelector("#box-1");
+    var taskDesc = document.querySelector("#box-2");
+    var startDate = document.querySelector("#box-3");
+    var endDate = document.querySelector("#box-4");
 
     var createdOn = new Date();
+    createdOn = createdOn.toDateString();
 
-    li.appendChild(addTaskItem("h4", taskTitle));
-    li.appendChild(addTaskItem("h5", taskDesc));
-    li.appendChild(addTaskItem("p", "Start Date : " + startDate));
-    li.appendChild(addTaskItem("p", "End Date : " + endDate));
-    li.appendChild(deleteBtn);
-    li.appendChild(editBtn);
+    taskObject.addTask(taskTitle.value, taskDesc.value, startDate.value, endDate.value, createdOn);
 
-    li.appendChild(addTaskItem("p", "Created On : " + createdOn.toDateString()));
+    showTask();
+    saveTask();
 
-    ul.appendChild(li);
+    taskTitle.value = "";
+    taskDesc.value = "";
+    startDate.value = "";
+    endDate.value = "";
+    
 }
+
+function showTask() {
+    var ul = document.querySelector("#taskList");
+    ul.innerHTML = "";
+
+    taskObject.taskArray.forEach(function(task) {
+        var li = document.createElement("li");
+
+        var deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete Task";
+        deleteBtn.className = "btn btn-danger";
+        deleteBtn.setAttribute("title", task.taskId);
+
+        var editBtn = document.createElement("button");
+        editBtn.innerText = "Edit Task";
+        editBtn.className = "btn btn-warning";
+        editBtn.setAttribute("title", task.taskId);
+
+        li.appendChild(addTaskItem("h4", task.taskTitle));
+        li.appendChild(addTaskItem("h5", task.taskDesc));
+        li.appendChild(addTaskItem("p", "Start Date : " + task.startDate));
+        li.appendChild(addTaskItem("p", "End Date : " + task.endDate));
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+
+        li.appendChild(addTaskItem("p", "Created On : " + task.createdOn));
+
+        ul.appendChild(li);
+
+        deleteBtn.addEventListener("click", deleteTask);
+    });
+
+}
+
+function deleteTask() {
+    var taskId = parseInt(this.title);
+    taskObject.deleteTask(taskId);
+    showTask();
+    saveTask();
+}
+
+function updateTask() {
+    taskObject.updateTask();
+}
+
+function saveTask() {
+    // stringify - will convert Array of object into a string
+    // because localstorage don't understand array and any other data type
+    // localstorage only understand string type of data
+    var data = JSON.stringify(taskObject.taskArray);
+    localStorage.setItem("taskArray", data);
+}
+
+function loadTask() {
+    var data = localStorage.getItem("taskArray");
+    taskObject.taskArray = JSON.parse(data);
+    showTask();
+}
+
+bindEvents();
+loadTask();
--- Thu Lab/JavaScript/TaskManager/assets/javascripts/taskModel.js
+++ Thu Lab/JavaScript/TaskManager/assets/javascripts/taskModel.js
@@ -8,12 +8,50 @@
 
 // Introduced in ES2015
 class Task {
-    constructor(taskId, taskTitle, taskDesc, startDate, endDate) {
+    constructor(taskId, taskTitle, taskDesc, startDate, endDate, createdOn, status) {
         this.taskId = taskId;
         this.taskTitle = taskTitle;
         this.taskDesc = taskDesc;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.createdOn = createdOn;
+        this.status = status;
     }
 }
 
+
+var taskObject = {
+    taskArray : [],
+    taskId : 0,
+    taskStatus : "Open",
+
+    addTask : function(taskTitle, taskDesc, startDate, endDate, createdOn) {
+        this.taskId++;
+        var task = new Task(this.taskId, taskTitle, taskDesc, startDate, endDate, createdOn, this.taskStatus);
+        this.taskArray.push(task);
+        console.log(this.taskArray);
+    },
+
+    deleteTask : function(id) {
+        // console.log(id);
+        // this.taskArray = this.taskArray.filter(function(obj) {
+        //     return obj.taskId != id;
+        // });
+
+        this.taskArray = this.taskArray.filter((obj) => {
+            return obj.taskId != id;
+        });
+    },
+
+    updateTask : function() {
+
+    },
+
+    searchTask : function() {
+
+    },
+
+    sortTask : function() {
+        // taskObject.taskArray.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+    }
+}

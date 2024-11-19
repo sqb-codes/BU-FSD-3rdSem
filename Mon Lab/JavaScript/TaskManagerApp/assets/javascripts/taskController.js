728cf63eac7b057592faf5af38c918b6f22a3988
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-11-19 09:16:34 +0530

    Updated code

--- Mon Lab/JavaScript/TaskManagerApp/assets/javascripts/taskController.js
+++ Mon Lab/JavaScript/TaskManagerApp/assets/javascripts/taskController.js
@@ -1,7 +1,8 @@
-window.addEventListener("load", bindEvents);
+// window.addEventListener("load", bindEvents);
 
 function bindEvents() {
     document.querySelector("#addTaskBtn").addEventListener("click", addNewTask);
+    document.querySelector("#sortTask").addEventListener("click", sortTask);
 }
 
 function addTag(tagName, tagText, className="") {
@@ -12,9 +13,6 @@ function addTag(tagName, tagText, className="") {
 } 
 
 function addNewTask() {
-    var ul = document.querySelector("#taskList");
-    var li = document.createElement("li");
-    // li.className = "list-group-item";
 
     var taskTitle = document.querySelector("#taskTitle").value;
     var taskDesc = document.querySelector("#taskDesc").value;
@@ -24,32 +22,67 @@ function addNewTask() {
     var createdOn = new Date();
     createdOn = createdOn.toDateString();
 
-    li.appendChild(addTag("h4", taskTitle));
-    li.appendChild(addTag("p", taskDesc));
-    li.appendChild(addTag("span", "Start Date : " + startDate, "d-block"));
-    li.appendChild(addTag("span", "End Date : " + endDate, "d-block"));
-    // li.appendChild(addTag("button", "Delete Task", "btn btn-danger"));
-    // li.appendChild(addTag("button", "Edit Task", "btn btn-warning"));
+    taskObject.addTask(taskTitle, taskDesc, startDate, endDate, createdOn);
+    showTask();
+    saveTask();
 
-    var deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete Task";
-    deleteBtn.className = "btn btn-danger";
+}
 
-    var editBtn = document.createElement("button");
-    editBtn.innerText = "Edit Task";
-    editBtn.className = "btn btn-warning";
+function showTask() {
+    var ul = document.querySelector("#taskList");
+    ul.innerHTML = "";
 
-    li.appendChild(deleteBtn);
-    li.appendChild(editBtn);
+    // Use forEach when you have array of object
+    taskObject.taskArray.forEach(function(obj) {
+        var li = document.createElement("li");
+        li.appendChild(addTag("h4", obj.title));
+        li.appendChild(addTag("p", obj.desc));
+        li.appendChild(addTag("span", "Start Date : " + obj.startDate, "d-block"));
+        li.appendChild(addTag("span", "End Date : " + obj.endDate, "d-block"));
 
-    li.appendChild(addTag("p", "Created On : " + createdOn));
+        var deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete Task";
+        deleteBtn.className = "btn btn-danger";
+        deleteBtn.setAttribute("title", obj.id);
 
-    ul.appendChild(li);
+        var editBtn = document.createElement("button");
+        editBtn.innerText = "Edit Task";
+        editBtn.className = "btn btn-warning";
 
-    deleteBtn.addEventListener("click", deleteTask);
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+        li.appendChild(addTag("p", "Created On : " + obj.createdOn));
 
+        ul.appendChild(li);
+
+        deleteBtn.addEventListener("click", deleteTask);
+    })
 }
 
 function deleteTask() {
-    
+    var id = this.title;
+    taskObject.deleteTask(id);
+    showTask();
+    saveTask();
 }
+
+function sortTask() {
+    var sortCriteria = document.querySelector("#sortCriteria").value;
+    var sortOrder = document.querySelector("#sortOrder").value;
+    taskObject.sortTask(sortCriteria, sortOrder);
+    showTask();
+}
+
+function saveTask() {
+    var data = JSON.stringify(taskObject.taskArray);
+    localStorage.setItem("taskListData",data);
+}
+
+// IIFE
+(function() {
+    var data = localStorage.getItem("taskListData");
+    taskObject.taskArray = JSON.parse(data);
+    showTask();
+})();
+
+bindEvents();
--- Mon Lab/JavaScript/TaskManagerApp/assets/javascripts/taskModel.js
+++ Mon Lab/JavaScript/TaskManagerApp/assets/javascripts/taskModel.js
@@ -24,5 +24,39 @@ class Task {
 // Object() - parent of all classes and function
 var taskObject = {
     taskArray : [],
-    taskId : 0
+    taskId : 0,
+    status : "Open",
+    addTask : function(title, desc, startDate, endDate, createdOn) {
+        this.taskId++;
+        var task = new Task(this.taskId, title, desc, startDate, endDate, createdOn, this.status);
+        this.taskArray.push(task);
+        console.log(this.taskArray);
+    },
+
+    deleteTask : function(id) {
+        this.taskArray = this.taskArray.filter(function(obj) {
+            return obj.id != id;
+        });
+    },
+
+    updateTask : function() {
+
+    },
+
+    sortTask : function(sortCriteria, sortOrder) {
+        if(sortOrder == "asc") {
+            taskObject.taskArray = taskObject.taskArray.sort(function(a, b) {
+                return new Date(a[sortCriteria]) - new Date(b[sortCriteria]);
+            });
+        }
+        else  {
+            taskObject.taskArray = taskObject.taskArray.sort(function(a, b) {
+                return new Date(b[sortCriteria]) - new Date(a[sortCriteria]);
+            })
+        }
+    },
+
+    searchTask : function() {
+
+    }
 }
